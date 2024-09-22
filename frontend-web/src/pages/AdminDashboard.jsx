cc574ea695d1f9a49345762533f7822e5688c377
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2024-09-22 10:03:07 +0530

    order

--- frontend-web/src/pages/AdminDashboard.jsx
+++ frontend-web/src/pages/AdminDashboard.jsx
@@ -4,13 +4,24 @@ import Card from 'react-bootstrap/Card';
 
 const AdminDashboard = () => {
   return (
-    <div className="mt-5">
+    <div className="mt-5 d-flex gap-3">
       <Link to="/createVendor">
         <Card style={{ width: '15rem' }}>
           <Card.Body className="d-flex justify-content-center align-items-center">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <i className="bi bi-person-lines-fill" style={{ fontSize: '2rem' }}></i>
               <p className="text-center">Vendor Management</p>
+            </div>
+          </Card.Body>
+        </Card>
+      </Link>
+
+      <Link to="/allOrders">
+        <Card style={{ width: '15rem' }}>
+          <Card.Body className="d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <i class="bi bi-box-fill" style={{ fontSize: '2rem' }}></i>
+              <p className="text-center">Order Management</p>
             </div>
           </Card.Body>
         </Card>
---
+++ frontend-web/src/pages/AllOrder.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Badge from 'react-bootstrap/Badge';
+
+
+const AllOrder = () => {
+
+    const [orders, setOrders] = useState(
+        [
+            {   
+                id:'1',
+                order_id: "Order_001",
+                amount: "Rs.200.00",
+                date: "9/8/2024",
+                delivery_code: "dl##1",
+                vendor_id:"ven001",
+                quantity: 4,
+                status: "Pending"
+            },
+            {   
+                id:'2',
+                order_id: "Order_002",
+                amount: "Rs.500.00",
+                date: "9/9/2024",
+                delivery_code: "dl##2",
+                vendor_id:"ven002",
+                quantity: 2,
+                status: "Delivered"
+            },
+            {   
+                id:'3',
+                order_id: "Order_003",
+                amount: "Rs.150.00",
+                date: "9/10/2024",
+                delivery_code: "dl##3",
+                vendor_id:"ven003",
+                quantity: 1,
+                status: "Partially delivered"
+            }
+        ]
+    )
+
+    //change status when toggling
+    const handleStatusChanging = (id,status) => {
+        setOrders(orders.map(order =>
+            order.id ===id ? {...order,status} : order
+        ))
+    }
+    
+    return (
+        <Container className='mt-4'>
+            <div className='d-flex align-items-center justify-content-center'>
+                <p></p>
+                <h4>All Orders</h4>
+            </div>
+            <Table striped bordered hover className='mt-2'>
+                <thead>
+                    <tr>
+                        <th>Order Id</th>
+                        <th>Amount</th>
+                        <th>Order Date</th>
+                        <th>Product Id</th>
+                        <th>Vendor Id</th>
+                        <th>Quantity</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orders.length > 0 ? orders.map((order, index) => (
+                            <tr key={index}>
+                                <td>{order.order_id}</td>
+                                <td>{order.amount}</td>
+                                <td>{order.date}</td>
+                                <td>{order.delivery_code}</td>
+                                <td>{order.vendor_id}</td>
+                                <td>{order.quantity}</td>
+                                <td className='d-flex align-items-center'>
+                                    <Dropdown className='text-center'>
+                                        <Dropdown.Toggle variant='info' style={{ backgroundColor: "white"}} className='dropdown_btn' id="dropdown-basic">
+                                            <Badge bg={order.status == "Pending" ? "danger" : order.status === "Delivered" ? "success" : "warning"} className='p-1'>
+                                                {order.status} <i className="fa-solid fa-angle-down"></i>
+                                            </Badge>
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item onClick={() => handleStatusChanging(order.id,"Pending")}>Pending</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => handleStatusChanging(order.id,"Delivered")}>Delivered</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => handleStatusChanging(order.id,"Partially delivered")}>Partially delivered</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </td>
+                            </tr>
+                        )) : <tr><td>No orders were found</td></tr>
+                    }
+                </tbody>
+            </Table>
+        </Container>
+    )
+}
+
+export default AllOrder
