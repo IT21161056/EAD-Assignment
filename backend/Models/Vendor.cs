cc1484a6e94ac86e4f35ff276575db55d9128c3b
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-09-24 21:09:55 +0530

    vendor model created

---
+++ backend/Models/Vendor.cs
@@ -0,0 +1,25 @@
+using System;
+using AspNetCore.Identity.MongoDbCore.Models;
+using Microsoft.AspNetCore.Identity;
+using MongoDbGenericRepository.Attributes;
+
+namespace backend.Models{
+
+    [CollectionName("Vendors")]
+    public class Vendor : MongoIdentityUser<Guid>{
+
+        [PersonalData]
+        public string VendorName {get; set;}
+
+        [PersonalData]
+        public string VendorEmail {get; set;}
+
+        [PersonalData]
+        public int VendorPhone {get; set;}
+
+        public string VendorAddress {get; set;}
+
+        public string VendorCity {get; set;}
+    }
+}
+
