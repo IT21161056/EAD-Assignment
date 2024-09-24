ae58259810bebf20358ea541f5c08894d9e2bfb4
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-09-24 10:48:36 +0530

    cart modal created for review

--- frontend-web/src/components/ui/nav/navbar.jsx
+++ frontend-web/src/components/ui/nav/navbar.jsx
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Badge from 'react-bootstrap/Badge';
+import Badge from "react-bootstrap/Badge";
+import { useCartContext } from "../../providers/ContextProvider";
+import CartModal from "./CartModal";
 
 const MainNavbar = () => {
+  const { itemCount } = useCartContext();
+  const [showCartModal, setShowCartModal] = useState(false);
+
+  const hanldeOpenModal = () => setShowCartModal(true);
+  const handleCloseModal = () => setShowCartModal(false);
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#" className="me-5">E-com</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="gap-3 d-flex align-items-baseline">
-            <Nav.Link href="/">Products</Nav.Link>
-            <Nav.Link href="/orders">Orders</Nav.Link>
-            <Nav.Link href="/listings">Listings</Nav.Link>
-            <Nav.Link href="/users">User Management</Nav.Link>
-            <Nav.Link href="/cart">
-            <i className="bi bi-bag-check" style={{fontSize:'23px'}}></i>
-            <Badge bg="secondary" className="position-absolute top-4">0</Badge>
-            </Nav.Link>
-            <NavDropdown title="Dashboard" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/adminDashboard">Admin</NavDropdown.Item>
-              <NavDropdown.Item href="/csr">CSR</NavDropdown.Item>
-              <NavDropdown.Item href="/vendor">Vendor</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#" className="me-5">
+            E-com
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="gap-3 d-flex align-items-baseline">
+              <Nav.Link href="/">Products</Nav.Link>
+              <Nav.Link href="/orders">Orders</Nav.Link>
+              <Nav.Link href="/listings">Listings</Nav.Link>
+              <Nav.Link href="/users">User Management</Nav.Link>
+              <button style={{ border: "none" }} onClick={hanldeOpenModal}>
+                <i className="bi bi-bag-check" style={{ fontSize: "23px" }}></i>
+                <Badge bg="secondary" className="position-absolute top-4">
+                  {itemCount}
+                </Badge>
+              </button>
+              <NavDropdown title="Dashboard" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/adminDashboard">
+                  Admin
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/csr">CSR</NavDropdown.Item>
+                <NavDropdown.Item href="/vendor">Vendor</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <CartModal show={showCartModal} onClose={handleCloseModal} />
+    </>
   );
 };
 
