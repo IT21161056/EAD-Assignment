be8fa9acd17c61dcbbda95348f3b9c7edd5cdc18
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-10-01 20:49:01 +0530

    update vendor dashboard

---
+++ frontend-web/src/pages/dashboradPages/UpdateVendor.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UpdateVendor = () => {
+  return (
+    <>
+      <div>
+        <h3>Update Vendor Details</h3>
+      </div>
+    </>
+  );
+};
+
+export default UpdateVendor;
