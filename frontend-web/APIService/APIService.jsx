af0e30e6412eb467f1b0582a2244d7ae74f64d92
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-10-01 19:34:08 +0530

    apis added for vendor

--- frontend-web/APIService/APIService.jsx
+++ frontend-web/APIService/APIService.jsx
@@ -1,20 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 import { BASE_URL } from "./BASEUrl";
 
 class APIService {
-    
-    //order endpoints
-    purchaseOrder(orderObj){
-        return axios.post(`${BASE_URL}/api/order`,orderObj)
-    }
+  // Order end points
 
-    getAllOrders(){
-        return axios.get(`${BASE_URL}/api/order`)
-    }
+  purchaseOrder(orderObj) {
+    return axios.post(`${BASE_URL}/api/order`, orderObj);
+  }
 
-    updateOrderDetails(updateOrderObj,id){
-        return axios.put(`${BASE_URL}/api/order/${id}`,updateOrderObj)
-    }
+  getAllOrders() {
+    return axios.get(`${BASE_URL}/api/order`);
+  }
+
+  updateOrderDetails(updateOrderObj, id) {
+    return axios.put(`${BASE_URL}/api/order/${id}`, updateOrderObj);
+  }
+
+  // Vendor end points
+
+  getAllVendors(config = {}) {
+    return axios.get(`${BASE_URL}/api/vendor`, config);
+  }
+
+  addVendor() {}
+
+  updateVendor() {}
 }
 
-export default new APIService;
+export default new APIService();
